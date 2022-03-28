1ae6397557280d5bd4e8e41d2812d2ff489fde65
Aliaksei Lutskevich <lutskevich.alexey@gmail.com>
2022-03-28 18:17:32 +0300

    v3.6

--- src/test/java/com/epam/tests/listeners/AllureListener.java
+++ src/test/java/com/epam/tests/listeners/AllureListener.java
@@ -2,22 +2,22 @@ package com.epam.tests.listeners;
 
 import static com.epam.tests.utils.ScreenshotUtils.takeScreenshotAndAttachToAllureReport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AllureListener implements TestWatcher {
 
-  private static final Logger LOG = LoggerFactory.getLogger(AllureListener.class);
+  private static final Logger LOG = LogManager.getLogger(AllureListener.class);
 
   public void testFailed(ExtensionContext context, Throwable cause) {
-    LOG.info("Test Failed for test {}: ", context.getDisplayName());
+    LOG.error("Test Failed for test {}: ", context.getDisplayName());
     takeScreenshotAndAttachToAllureReport();
   }
 
   public void testAborted(ExtensionContext context, Throwable cause) {
-    LOG.info("Test Aborted for test {}: ", context.getDisplayName());
+    LOG.warn("Test Aborted for test {}: ", context.getDisplayName());
     takeScreenshotAndAttachToAllureReport();
   }
 
